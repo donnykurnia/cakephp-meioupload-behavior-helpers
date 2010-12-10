d81a10082f3c4c9eced018096a43ca3a5f96d746
Donny Kurnia <donnykurnia@gmail.com>
2010-12-10 17:41:19 +0700

    add fileData options to be able to display file in relation loop
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- views/helpers/meio_upload.php
+++ views/helpers/meio_upload.php
@@ -20,10 +20,11 @@ class MeioUploadHelper extends AppHelper {
         'show_filesize' => false,
         'link_text' => 'Download file',
         'type' => 'file',
+        'full' => false,
     );
 
     function displayFile($fieldName, $options = array()) {
-        $this->setEntity($fieldName);
+        //$this->setEntity($fieldName);
 
         $options = array_merge(
             array('before' => null, 'between' => null, 'after' => null, 'format' => null),
@@ -32,9 +33,16 @@ class MeioUploadHelper extends AppHelper {
             $options
         );
 
-        $modelKey = $this->params['models'][0];
-        if ( ! isset($options['data']) ) {
-            $options['data'] = $this->data;
+        $data = $this->data;
+        if ( isset($options['data']) ) {
+            $data = $options['data'];
+        }
+        if ( isset($this->params['models']) AND is_array($this->params['models']) ) {
+            $modelKey = $this->params['models'][0];
+            $fileData = $data[$modelKey];
+        }
+        if ( isset($options['fileData']) ) {
+            $fileData = $options['fileData'];
         }
 
         $divOptions = array();
@@ -77,7 +85,7 @@ class MeioUploadHelper extends AppHelper {
 			if (substr($alt, -3) == '_id') {
 				$alt = substr($alt, 0, strlen($alt) - 3);
 			}
-			$alt = __(Inflector::humanize(Inflector::underscore($alt)), true);
+			$alt = Inflector::humanize(Inflector::underscore($alt));
 		}
 
         if ($label !== false) {
@@ -95,16 +103,16 @@ class MeioUploadHelper extends AppHelper {
         unset($options['before'], $options['between'], $options['after'], $options['format']);
 
         $input = '';
-        if ( isset($options['data'][$modelKey][$fieldName]) AND ! is_array($options['data'][$modelKey][$fieldName]) AND $options['data'][$modelKey][$fieldName] != '' ) {
-            $is_image = substr($options['data'][$modelKey][$options['fields']['mimetype']], 0, 5) == 'image';
-            $file_url = '/'.str_replace('\\', '/', $options['data'][$modelKey][$options['fields']['dir']]).'/'.$options['data'][$modelKey][$fieldName];
-            $file_path = WWW_ROOT.$options['data'][$modelKey][$options['fields']['dir']].DS.$options['data'][$modelKey][$fieldName];
+        if ( isset($fileData[$fieldName]) AND ! is_array($fileData[$fieldName]) AND $fileData[$fieldName] != '' ) {
+            $is_image = substr($fileData[$options['fields']['mimetype']], 0, 5) == 'image';
+            $file_url = ($options['full'] ? FULL_BASE_URL : '').$this->webroot.str_replace('\\', '/', $fileData[$options['fields']['dir']]).'/'.$fileData[$fieldName];
+            $file_path = WWW_ROOT.$fileData[$options['fields']['dir']].DS.$fileData[$fieldName];
             //check the thumbnail
             if ( $options['thumbsize'] != 'normal' ) {
-                $thumb_file_path = WWW_ROOT.$options['data'][$modelKey][$options['fields']['dir']].DS.'thumb.'.$options['thumbsize'].'.'.$options['data'][$modelKey][$fieldName];
+                $thumb_file_path = WWW_ROOT.$fileData[$options['fields']['dir']].DS.'thumb.'.$options['thumbsize'].'.'.$fileData[$fieldName];
                 if (is_readable($thumb_file_path) ) {
                     $file_path = $thumb_file_path;
-                    $file_url = '/'.$options['data'][$modelKey][$options['fields']['dir']].'/'.'thumb.'.$options['thumbsize'].'.'.$options['data'][$modelKey][$fieldName];
+                    $file_url = ($options['full'] ? FULL_BASE_URL : '').$this->webroot.$fileData[$options['fields']['dir']].'/'.'thumb.'.$options['thumbsize'].'.'.$fileData[$fieldName];
                 }
             }
             if (is_readable($file_path) ) {
@@ -112,7 +120,7 @@ class MeioUploadHelper extends AppHelper {
                     $input = $this->Html->image($file_url, array('alt' => $alt));
                 }
                 else {
-                    $link_text = $options['show_realname'] ? $options['data'][$modelKey][$options['fields']['realname']] : __($options['link_text'], true);
+                    $link_text = $options['show_realname'] ? $fileData[$options['fields']['realname']] : $options['link_text'];
                     $input = $this->Html->link($link_text, $file_url);
                 }
             }
